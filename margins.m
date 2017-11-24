ddb8a8e49a6f5e07deff576cff48bbf445120122
FDS1990 <f.disales@hotmail.com>
2017-11-24 20:16:32 +0100

    ok

--- margins.m
+++ margins.m
@@ -4,4 +4,4 @@ numeratore=s;
 denominatore=s+1;
 
 function_test=numeratore/denominatore;
-margin(function_test); //see in which point there's the critic pulsation + define the gain margin and the phase margin
+margin(function_test); 
